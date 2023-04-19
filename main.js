d44c6b71d18764f31dbc2b1fc284396dde4ebce1
Kamil Sajdera <kamilsajdera@gmail.com>
2023-04-19 23:16:21 +0200

    Release v2

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const glad = "Im glad you're here.";
 let napis = "";
 let typing;
 
-let wrapper = $("")
+let wrapper;
 
 $(function() {
 
@@ -25,7 +25,7 @@ $(function() {
         }, 0100);
 
         $(".topBar_navigate ul").css("visibility", "visible")
-    }, 0700);
+    }, 1200);
     
     
 });
@@ -80,11 +80,16 @@ $("#slide-prev").click(() => {
 })
 
 
+let bar_height = 0;
+let block = false;
 
 $(window).on('scroll', function () {
     
-    const bgWrapper = document.getElementById("mainWrapper");
-
+    const bgWrapper = $("#mainWrapper");
+    const about = $("#aboutMe").offset().top;
+    const services = $("#services").offset().top;
+    const myWorks = $("#myWorks").offset().top;
+    
 
     let scrollValue = $(window).scrollTop();
 
@@ -95,8 +100,97 @@ $(window).on('scroll', function () {
 
 
     /// paralax on main header
-    bgWrapper.style.top = scrollValue * 0.5 + 'px';
+    bgWrapper.css("top", `${scrollValue * 0.5}px`);
+
+
+    /// chart bars growing
+    if(scrollValue+300 >= $(".chartWrapper").offset().top)
+    {
+        if(block)
+            return;
+
+        setInterval(() => {
+            if(bar_height++ >= 80)
+                return;
+
+            $(".chartWrapper_bars__item").eq(1).css("height", bar_height+"%")
+
+            if(bar_height <= 75)
+                $(".chartWrapper_bars__item").eq(0).css("height", bar_height+"%")
+
+            if(bar_height <= 55)
+                $(".chartWrapper_bars__item").eq(2).css("height", bar_height+"%")
+
+            if(bar_height <= 20)
+                $(".chartWrapper_bars__item").eq(3).css("height", bar_height+"%")
+    
+        }, 1000/60);
+
+        block = true;
+    }
+
+
+
+    /// active item in menu \
+
+    if(scrollValue+100 < about)
+        $(".topBar_navigate ul li a").eq(0).addClass("active-item");
+    else 
+        $(".topBar_navigate ul li a").eq(0).removeClass("active-item");
+
+
+    if(scrollValue+100 >= about && scrollValue+100 < services)
+        $(".topBar_navigate ul li a").eq(1).addClass("active-item");
+    else 
+        $(".topBar_navigate ul li a").eq(1).removeClass("active-item");
+
+
+    if(scrollValue+100 >= services && scrollValue+100 < myWorks)
+        $(".topBar_navigate ul li a").eq(2).addClass("active-item");
+    else 
+        $(".topBar_navigate ul li a").eq(2).removeClass("active-item");
+
+    if(scrollValue+100 >= myWorks)
+        $(".topBar_navigate ul li a").eq(3).addClass("active-item");
+    else 
+        $(".topBar_navigate ul li a").eq(3).removeClass("active-item");
+
+
 
 
     
 });
+
+$(".myWorks_item").mouseover(function() { 
+
+    $(this).find("img").css("transform", "scale(1.1)");
+    $(this).find(".myWorks_item__overlay").css("opacity", "1");
+
+    setTimeout(() => {
+        $(this).find(".myWorks_item__overlay_code").css("opacity", "1")
+        $(this).find(".myWorks_item__overlay_code").css("top", "90px")
+
+        $(this).find(".myWorks_item__overlay_demo").css("opacity", "1")
+        $(this).find(".myWorks_item__overlay_demo").css("bottom", "60px")
+    }, 0250);
+    
+  });
+  
+  $(".myWorks_item").mouseleave(function() { 
+
+    $(this).find("img").css("transform", "scale(1)");
+    $(this).find(".myWorks_item__overlay").css("opacity", "0");
+
+    $(this).find(".myWorks_item__overlay_code").css("opacity", "0")
+    $(this).find(".myWorks_item__overlay_code").css("top", "0px")
+
+    $(this).find(".myWorks_item__overlay_demo").css("opacity", "0")
+    $(this).find(".myWorks_item__overlay_demo").css("bottom", "0px")
+
+  });
+
+
+
+
+
+  
